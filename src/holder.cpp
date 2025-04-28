152e64e5766b522a5d0bf7cc5f9221dafacb2ff7
navid-atashbar <atashbar@uci.edu>
2025-04-28 20:29:26 +0000

    added is_empty and is_full functions for array stack

--- src/holder.cpp
+++ src/holder.cpp
@@ -7,13 +7,17 @@ ostream & operator << (ostream & out, Stack & L);
         buf = new string [capacity];
     }
     
-    void push(const string & word){
+    void ArrayStack::push(const string & word){
         
     }
     void pop();
     string top();
-    bool is_empty();
-    bool is_full();
+    bool is_empty(){
+        return (size == 0);
+    }
+    bool is_full(){
+        return (size==capacity);
+    }
     void print(ostream & out);
     ~ArrayStack();
     
