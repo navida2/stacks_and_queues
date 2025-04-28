5d780e175bf5d2a8dfdd2abacf5caf891c054b7b
navid-atashbar <atashbar@uci.edu>
2025-04-28 20:27:52 +0000

    addd array stack constructor

--- src/holder.cpp
+++ src/holder.cpp
@@ -2,8 +2,14 @@
 ostream & operator << (ostream & out, Stack & L);
 
 //Array stack functions
-    ArrayStack(int cap = NWORDS);
-    void push(const string & word);
+
+    ArrayStack::ArrayStack(int cap):Stack("Array Stack"), capacity(cap), size(0){
+        buf = new string [capacity];
+    }
+    
+    void push(const string & word){
+        
+    }
     void pop();
     string top();
     bool is_empty();
