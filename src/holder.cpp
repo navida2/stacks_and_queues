7ef0c14f5730ca6c91edc5a303a323de29bd722c
navid-atashbar <atashbar@uci.edu>
2025-04-28 20:32:52 +0000

    added print statement fucntionality and top()

--- src/holder.cpp
+++ src/holder.cpp
@@ -11,13 +11,20 @@ ostream & operator << (ostream & out, Stack & L);
         
     }
     void pop();
-    string top();
+    string top(){
+        //come back and add error if list is empty
+        return buf[size-1];
+    }
     bool is_empty(){
         return (size == 0);
     }
     bool is_full(){
         return (size==capacity);
     }
-    void print(ostream & out);
+    void print(ostream & out){
+        for(int i=0;i<size;++i){
+            out<<buf[i]<<endl;
+        }
+    }
     ~ArrayStack();
     
