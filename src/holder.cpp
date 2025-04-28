e6c97fc4562c8da3d61c749461ba2103fe53b22e
navid-atashbar <atashbar@uci.edu>
2025-04-28 20:38:05 +0000

    fixed tabs

--- src/holder.cpp
+++ src/holder.cpp
@@ -3,36 +3,35 @@ ostream & operator << (ostream & out, Stack & L);
 
 //Array stack functions
 
-    ArrayStack::ArrayStack(int cap):Stack("Array Stack"), capacity(cap), size(0){
-        buf = new string [capacity];
+ArrayStack::ArrayStack(int cap):Stack("Array Stack"), capacity(cap), size(0){
+    buf = new string [capacity];
+}
+
+void ArrayStack::push(const string & word){
+    if(size!=capacity){
+        buf[size++] = word;
     }
-    
-    void ArrayStack::push(const string & word){
-        if(size!=capacity){
-            buf[size++] = word;
-        }
+}
+void ArrayStack::pop(){
+    if(size!=0){
+        --size;
     }
-    void ArrayStack::pop(){
-        if(size!=0){
-            --size;
-        }
+}
+string top(){
+    //come back and add error if list is empty
+    return buf[size-1];
+}
+bool is_empty(){
+    return (size == 0);
+}
+bool is_full(){
+    return (size==capacity);
+}
+void print(ostream & out){
+    for(int i=0;i<size;++i){
+        out<<buf[i]<<endl;
     }
-    string top(){
-        //come back and add error if list is empty
-        return buf[size-1];
-    }
-    bool is_empty(){
-        return (size == 0);
-    }
-    bool is_full(){
-        return (size==capacity);
-    }
-    void print(ostream & out){
-        for(int i=0;i<size;++i){
-            out<<buf[i]<<endl;
-        }
-    }
-    ArrayStack::~ArrayStack(){
-        delete[] buf;
-    }
-    
+}
+ArrayStack::~ArrayStack(){
+    delete[] buf;
+}
