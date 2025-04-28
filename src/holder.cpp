071724bdb898d8fe1d428d95e04241c4d5644728
navid-atashbar <atashbar@uci.edu>
2025-04-28 20:39:30 +0000

    added stream operator and bug fixes

--- src/holder.cpp
+++ src/holder.cpp
@@ -1,5 +1,8 @@
 
-ostream & operator << (ostream & out, Stack & L);
+ostream & operator << (ostream & out, Stack & L){
+    L.print(out);
+    return out;
+}
 
 //Array stack functions
 
@@ -17,17 +20,17 @@ void ArrayStack::pop(){
         --size;
     }
 }
-string top(){
+string ArrayStack::top(){
     //come back and add error if list is empty
     return buf[size-1];
 }
-bool is_empty(){
+bool ArrayStack::is_empty(){
     return (size == 0);
 }
-bool is_full(){
+bool ArrayStack::is_full(){
     return (size==capacity);
 }
-void print(ostream & out){
+void ArrayStack::print(ostream & out){
     for(int i=0;i<size;++i){
         out<<buf[i]<<endl;
     }
