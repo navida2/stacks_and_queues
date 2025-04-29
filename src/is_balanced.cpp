25ade58c79f5acc43ac3b3f8743a5816b0bb6149
navid-atashbar <atashbar@uci.edu>
2025-04-29 05:26:39 +0000

    added happy path for is_balanced

--- src/is_balanced.cpp
+++ src/is_balanced.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <stack>
+#include <iostream>
 using namespace std;
 
 void error(string msg, char c);
 bool is_balanced(string s){
     stack<char> bal;
-    for (auto s: c){
+    for (auto c: s){
         if(c=='(' || c== '<'||c=='{' || c=='['){
             //[push it onto stack]
             bal.push(c);
         }
         else if (c==')' || c== '>'||c=='}' || c==']'){
             //CHECK IF THE ELEMENT MATCHES ITS OTHER OPENING ONE IF IT DOES THEN POP ELSE FALSE
-            char top = bal.top();
+            
             //get elem then pop it
             //check if the elem before is its closing one if not false
+            
+            char top = bal.top();
             bal.pop();
-            if (top =='(' &&)
+            if (!(top =='(' && c ==')') || !(top=='<'&& c=='>')|| !(top=='{' && c =='}') || !(top=='[' && c ==']')){
+                return false;
+            }
 
         }
-
+    
     }
+    return true;
 }
-void test_is_balanced();
+void test_is_balanced(){
+    cout<<
+}
